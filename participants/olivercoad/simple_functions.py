6bd82358bebc185810a36d4aaaf4d10827c16d3c
Oliver Coad <oliver.coad@gmail.com>
2021-04-28 11:06:19 +1000

    Add is_prime function

--- participants/olivercoad/simple_functions.py
+++ participants/olivercoad/simple_functions.py
@@ -10,3 +10,17 @@ def factorial(value):
         return 1
     else:
         return value * factorial(value - 1)
+
+# from https://en.wikipedia.org/wiki/Primality_test#Python_code
+def is_prime(n: int) -> bool:
+    """Primality test using 6k+-1 optimization."""
+    if n <= 3:
+        return n > 1
+    if n % 2 == 0 or n % 3 == 0:
+        return False
+    i = 5
+    while i ** 2 <= n:
+        if n % i == 0 or n % (i + 2) == 0:
+            return False
+        i += 6
+    return True

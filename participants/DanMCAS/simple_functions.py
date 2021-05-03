f91e11d11466fb5d7681e05bb2ee07b587a2df95
Daniel McPherson <dmcpherson0@gmail.com>
2021-05-03 16:26:40 +1000

    I made some changes but I didnt finish so this doesnt work

--- participants/DanMCAS/simple_functions.py
+++ participants/DanMCAS/simple_functions.py
@@ -9,4 +9,20 @@ def factorial(value):
     if value == 0:
         return 1
     else:
-        return value * factorial(value - 1)
+        return value * factorial(value - 1
+
+def isprime(value):    
+    print("I didn't finish this so thats why it doesn't work")
+    if value == 1:
+        print("Nice try, buddy. That aint prime")
+    elif value == 2:
+        print("Great things can come from even the littlest prime :)")
+    else:
+        divisor = 2
+        for divisor < value:
+            if value % divisor == 0:
+                print("I'm so sorry to tell you that this number is not prime :(")
+            else:
+                divisor += 1
+      
+        
